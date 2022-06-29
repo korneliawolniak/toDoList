3a2fec3bfe470ceda199b1f358e78f4978441e4f
korneliawolniak <korneliawolniak@gmail.com>
2022-06-29 12:12:28 +0200

    created components and routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './services/app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { DashboardPageComponent } from '../pages/dashboard-page/dashboard-page.component';
+import { ErrorPageComponent } from '../pages/error-page/error-page.component';
+import { LoginPageComponent } from '../pages/login-page/login-page.component';
+import { RegisterPageComponent } from '../pages/register-page/register-page.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardPageComponent,
+  },
+  {
+    path: 'error',
+    component: ErrorPageComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'error',
+  },
+];
